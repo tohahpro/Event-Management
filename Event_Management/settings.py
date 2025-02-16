b00bd474474f1a00504700e7a79271395278d132
Toha Hossain <tohahpro@gmail.com>
2025-02-16 20:07:01 +0600

    database connect complete

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'Event_Management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_managements',
+        'NAME': 'event-managements',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
